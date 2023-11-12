8d76ff3eecba06e4797cb602cf8fdcb9e8555c46
Ihor Hural <ihorhural@MacBook-Air-Igor.local>
2023-11-12 16:52:53 +0200

    fix slider

--- src/js/script.js
+++ src/js/script.js
@@ -2,82 +2,19 @@ const swiper = new Swiper('.swiper2', {
   direction: 'horizontal',
   loop: true,
   slidesPerView: 1,
-  speed: 900,
+  speed: 1300,
   effect: 'flip',
-  autolay: {
-    delay: 1000,
+  slideToClickedSlide: true,
+  autoplay: {
+    delay: 2300,
   },
   breakpoints: {
     1024: {
       limitRotation: false,
-      speed: 100,
+      speed: 2300,
     },
   }
 });
-// window.addEventListener('resize', function () {
-//   const promo = document.querySelector(".promo");
-//   const rswiper = document.querySelector('.swiper1');
-
-//   if (document.documentElement.clientWidth >= 1024) {
-//     if (!rswiper) {
-//       promo.insertAdjacentHTML('beforeend',
-//         ` <div class="swiper swiper1">
-
-//           <div class="swiper-wrapper">
-
-//               <div class="swiper-slide slide-fon">
-//                   <img src="img/slider2/1.jpeg" alt="">
-//               </div>
-
-//               <div class="swiper-slide slide-fon">
-//                   <img src="img/slider2/2.jpg" alt="">
-//               </div>
-
-//               <div class="swiper-slide slide-fon">
-//                   <img src="img/slider2/3.jpg" alt="">
-//               </div>
-
-//           </div>
-
-//         <div class="swiper-pagination"></div>
-
-//       </div> `
-//       )
-//     } 
-//     // else if (rswiper) {
-//     //   rswiper.remove
-//     // }
-
-//   }
-
-// })
-
-// if (document.documentElement.clientWidth >= 1024) {
-//   const promo = document.querySelector(".promo");
-//   promo.insertAdjacentHTML('beforeend',
-//     ` <div class="swiper swiper1">
-
-//     <div class="swiper-wrapper">
-
-//         <div class="swiper-slide slide-fon">
-//             <img src="img/slider2/1.jpeg" alt="">
-//         </div>
-
-//         <div class="swiper-slide slide-fon">
-//             <img src="img/slider2/2.jpg" alt="">
-//         </div>
-
-//         <div class="swiper-slide slide-fon">
-//             <img src="img/slider2/3.jpg" alt="">
-//         </div>
-
-//     </div>
-
-//   <div class="swiper-pagination"></div>
-
-// </div> `)
-// }
-
 
 let allreadyCalled = false;
 
@@ -90,10 +27,7 @@ function init() {
         loop: true,
         slidesPerView: 1,
         effect: 'cube',
-        autolay: {
-          delay: 1000,
-        },
-        speed: 900,
+        speed: 2300,
         pagination: {
           el: '.swiper1 .swiper-pagination',
           clickable: true,
@@ -109,26 +43,26 @@ function init() {
 init()
 
 
-// let scrollpos = window.scrollY;
-// const promo = document.querySelector(".promo");
-// const header = document.querySelector("header");
-// const baner = document.querySelector(".baner");
-// const servicescroll = document.querySelector(".services .container .content");
+let scrollpos = window.scrollY;
+const promo = document.querySelector(".promo");
+const header = document.querySelector("header");
+const baner = document.querySelector(".baner");
+const servicescroll = document.querySelector(".services .container .content");
 
-// const scrollChange = header.clientHeight / 2;
-// const scrollToscroll = promo.clientHeight + baner.clientHeight + scrollChange;
+const scrollChange = header.clientHeight / 2;
+const scrollToscroll = promo.clientHeight + baner.clientHeight + scrollChange;
 
-// const add_class_on_scroll = (el, className) => el.classList.add(className);
-// const remove_class_on_scroll = (el, className) => el.classList.remove(className);
+const add_class_on_scroll = (el, className) => el.classList.add(className);
+const remove_class_on_scroll = (el, className) => el.classList.remove(className);
 
-// window.addEventListener('scroll', function () {
-//   if (document.documentElement.clientWidth >= 1024) {
+window.addEventListener('scroll', function () {
+  if (document.documentElement.clientWidth >= 1024) {
 
-//     scrollpos = window.scrollY;
-//     if (scrollpos >= scrollChange) {
-//       add_class_on_scroll(header, 'fix')
-//     } else {
-//       remove_class_on_scroll(header, 'fix')
-//     }
-//   }
-// })
+    scrollpos = window.scrollY;
+    if (scrollpos >= scrollChange) {
+      add_class_on_scroll(header, 'fix')
+    } else {
+      remove_class_on_scroll(header, 'fix')
+    }
+  }
+})
